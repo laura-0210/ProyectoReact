c02b8e45fa96694a27e48e1c32fcc06fcbb03b84
laura-0210 <laudelpozo.26@gmail.com>
2026-02-16 11:03:58 +0100

    cambios main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 # --- CONFIGURACIÓN DE CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8001",
+        "https://tu-app-en-vercel.vercel.app" # Reemplaza con tu URL real de Vercel
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
